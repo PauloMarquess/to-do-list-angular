ee796a24208175bf73281344286057e859157f8b
João Paulo <joao.elesbao@g.globo.com>
2022-11-06 18:31:30 -0300

    adicionando itemns no localStorage

--- src/app/modules/home/components/list/list.component.ts
+++ src/app/modules/home/components/list/list.component.ts
@@ -4,44 +4,43 @@ import { Component, DoCheck } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.sass']
+  styleUrls: ['./list.component.sass'],
 })
 export class ListComponent implements DoCheck {
+  taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]') ;
 
-  taskList:Array<TaskList> = [
-
-  ]
-
-  constructor() { }
+  constructor() {}
   ngDoCheck(): void {
- this.taskList.sort((first,last)=>Number(first.checked) - Number(last.checked)
-
- )
+    this.setLocalStorage();
   }
 
-
-
-  setEmitTaskList(event:string){
-    this.taskList.push({task:event,checked:false})
+  setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
   }
 
-  deleteItemTaskList (event:number){
-    this.taskList.splice(event,1)
+  deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
   }
-  deleteAllTaks () {
-    const confirm = window.confirm("Você deseja realmente Deletar tudo ? ")
-    if(confirm){
-      this.taskList= []
+  deleteAllTaks() {
+    const confirm = window.confirm('Você deseja realmente Deletar tudo ? ');
+    if (confirm) {
+      this.taskList = [];
     }
   }
-  validationInput (event:string,index:number){
-    if(!event.length){
-      const confirm = window.confirm('Task está vazia,deseja Deletar ?')
-      if(confirm){
-        this.deleteItemTaskList(index)
+  validationInput(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('Task está vazia,deseja Deletar ?');
+      if (confirm) {
+        this.deleteItemTaskList(index);
       }
     }
-
   }
-
+  setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort(
+        (first, last) => Number(first.checked) - Number(last.checked)
+      );
+      localStorage.setItem('list', JSON.stringify(this.taskList));
+    }
+  }
 }
